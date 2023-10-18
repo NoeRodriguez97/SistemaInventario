f95fc46dee7109565f921ea597ddf1bfd6e9cbd7
NoeRodriguez97 <noeadrians27@gmail.com>
2023-10-18 08:32:20 -0600

    Creacion del proyecto y separacion por proyectos

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        [MaxLength(60,ErrorMessage = "El Nombre debe ser maximo a 60 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La Descripcion es requerido")]
+        [MaxLength(60, ErrorMessage = "La descripcion debe ser maximo a 100 caracteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
